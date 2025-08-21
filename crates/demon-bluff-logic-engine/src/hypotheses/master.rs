0d54b6f35ecb680b83b9a2150a08a179a08636cb
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-21 00:48:40 -0400

    WIP

--- crates/demon-bluff-logic-engine/src/hypotheses/master.rs
+++ crates/demon-bluff-logic-engine/src/hypotheses/master.rs
@@ -1,18 +1,23 @@
-use std::ffi::os_str::Display;
+use std::fmt::{Display, write};
 
 use demon_bluff_gameplay_engine::game_state::{self, GameState};
 
 use crate::{
-    hypothesis::{Hypothesis, HypothesisReference, HypothesisRegistrar, HypothesisRepository},
+    hypothesis::{
+        EvaluationRequestFulfillment, EvaluationRequestResult, FitnessAndAction, Hypothesis,
+        HypothesisContainer, HypothesisReference, HypothesisRegistrar, HypothesisRepository,
+        HypothesisResult,
+    },
     player_action::PlayerAction,
 };
 
 use super::reveal::RevealHypothesis;
 
+#[derive(Debug)]
 struct MasterHypothesis {
-    reveal: HypothesisReference,
-    execute: HypothesisReference,
-    ability: HypothesisReference,
+    reveal_hypothesis: HypothesisReference,
+    execute_hypothesis: HypothesisReference,
+    ability_hypothesis: HypothesisReference,
 }
 
 impl MasterHypothesis {
@@ -20,11 +25,17 @@ impl MasterHypothesis {
         game_state: &GameState,
         registrar: &mut HypothesisRegistrar,
     ) -> HypothesisReference {
-        registrar.register(Box::new(Self {
-            reveal: RevealHypothesis::create(game_state, registrar),
-            execute: todo!(),
-            ability: todo!(),
+        registrar.register(HypothesisContainer::new(Self {
+            reveal_hypothesis: RevealHypothesis::create(game_state, registrar),
+            execute_hypothesis: todo!(),
+            ability_hypothesis: todo!(),
         }))
+    }
+}
+
+impl Display for MasterHypothesis {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Master Hypothesis")
     }
 }
 
@@ -33,11 +44,54 @@ impl Hypothesis for MasterHypothesis {
         &mut self,
         game_state: &GameState,
         repository: &mut HypothesisRepository,
-    ) -> Option<f64> {
-        todo!()
-    }
+    ) -> HypothesisResult {
+        let evaluator_result = repository.request_sub_evaluation(0.0);
+        let mut evaluator;
+        let mut conclusive;
+        match evaluator_result {
+            EvaluationRequestResult::Approved(hypothesis_evaluator) => {
+                evaluator = hypothesis_evaluator;
+                conclusive = None;
+            }
+            EvaluationRequestResult::BreakCycle(hypothesis_evaluator) => {
+                evaluator = hypothesis_evaluator;
+                conclusive = Some(true);
+            }
+        }
 
-    fn action(&self) -> std::collections::HashSet<PlayerAction> {
-        todo!()
+        let mut best_fitness = None;
+        let mut result = None;
+        take_max_fitness(
+            evaluator.sub_evaluate(&self.ability_hypothesis),
+            &mut best_fitness,
+            &mut conclusive,
+            &mut result,
+        );
+        take_max_fitness(
+            evaluator.sub_evaluate(&self.reveal_hypothesis),
+            &mut best_fitness,
+            &mut conclusive,
+            &mut result,
+        );
+        take_max_fitness(
+            evaluator.sub_evaluate(&self.execute_hypothesis),
+            &mut best_fitness,
+            &mut conclusive,
+            &mut result,
+        );
+
+        result.expect("result should have been populated!")
+    }
+}
+
+fn take_max_fitness(
+    sub_hypothesis_result: HypothesisResult,
+    best_fitness: &mut Option<FitnessAndAction>,
+    conclusive: &mut Option<bool>,
+    result: &mut Option<HypothesisResult>,
+) {
+    match sub_hypothesis_result {
+        HypothesisResult::Weak(fitness_and_action) => todo!(),
+        HypothesisResult::Conclusive(fitness_and_action) => todo!(),
     }
 }
--- crates/demon-bluff-logic-engine/src/hypothesis.rs
+++ crates/demon-bluff-logic-engine/src/hypothesis.rs
@@ -1,50 +1,95 @@
-use std::{cell::RefCell, collections::HashSet, fmt::Display};
+use std::{cell::RefCell, collections::HashSet, fmt::Debug};
 
 use demon_bluff_gameplay_engine::game_state::GameState;
 
 use crate::player_action::PlayerAction;
 
+#[derive(Debug)]
 pub struct HypothesisReference(usize);
 
-pub struct HypothesisRepository {
-    hypotheses: Vec<RefCell<Box<dyn Hypothesis>>>,
+pub struct HypothesisRepository {}
+
+pub struct HypothesisEvaluator {}
+
+pub enum EvaluationRequestResult {
+    Approved(HypothesisEvaluator),
+    BreakCycle(HypothesisEvaluator),
 }
 
-pub enum EvaluationRequestFulfillment {
-    Pending,
-    Ready(f64),
-    BreakCycle,
+pub enum HypothesisResult {
+    Weak(FitnessAndAction),
+    Conclusive(FitnessAndAction),
 }
 
-pub trait Hypothesis {
+pub struct FitnessAndAction {
+    action: HashSet<PlayerAction>,
+    fitness: f64,
+}
+
+pub struct HypothesisContainer {
+    hypothesis: RefCell<Box<dyn Hypothesis>>,
+    last_evaluate: Option<FitnessAndAction>,
+}
+
+pub trait Hypothesis: Debug {
     fn evaluate(
         &mut self,
         game_state: &GameState,
         repository: &mut HypothesisRepository,
-    ) -> Option<f64>;
-
-    fn action(&self) -> HashSet<PlayerAction>;
+    ) -> HypothesisResult;
 }
 
 pub struct HypothesisRegistrar {
     hypotheses: Vec<RefCell<Box<dyn Hypothesis>>>,
 }
 
+impl FitnessAndAction {
+    pub fn fitness(&self) -> f64 {
+        self.fitness
+    }
+
+    pub fn action(&self) -> &HashSet<PlayerAction> {
+        &self.action
+    }
+}
+
 impl HypothesisRepository {
-    pub fn request_evaluation<F>(
-        &mut self,
-        current_fitness: f64,
-        hypothesis_reference: &HypothesisReference,
-        f: F,
-    ) where
-        F: FnMut(&mut dyn Hypothesis) -> EvaluationRequestFulfillment,
-    {
+    pub fn request_sub_evaluation(&mut self, current_fitness: f64) -> EvaluationRequestResult {
+        todo!()
+    }
+}
+
+impl HypothesisEvaluator {
+    pub fn sub_evaluate(&mut self, hypothesis_reference: &HypothesisReference) -> HypothesisResult {
         todo!()
     }
 }
 
 impl HypothesisRegistrar {
-    pub fn register(&mut self, hypothesis: Box<dyn Hypothesis>) -> HypothesisReference {
+    pub fn register(&mut self, hypothesis: HypothesisContainer) -> HypothesisReference {
         todo!()
+    }
+}
+
+impl HypothesisResult {
+    pub fn conclusive(fitness: f64, action: PlayerAction) -> Self {
+        let mut set = HashSet::new();
+        set.insert(action);
+        Self::Conclusive(FitnessAndAction {
+            action: set,
+            fitness,
+        })
+    }
+}
+
+impl HypothesisContainer {
+    pub fn new<HypothesisImpl>(hypothesis: HypothesisImpl) -> Self
+    where
+        HypothesisImpl: Hypothesis + 'static,
+    {
+        Self {
+            hypothesis: RefCell::new(Box::new(hypothesis)),
+            last_evaluate: None,
+        }
     }
 }
