0b734fcc2839e2ba47d90c37115b1b8a9ef2ae91
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-27 07:20:05 -0400

    WIP

--- crates/demon-bluff-deducer/src/evaluator/components/game_state.rs
+++ crates/demon-bluff-deducer/src/evaluator/components/game_state.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use demon_bluff_gameplay_engine::game_state::GameState;
 
 #[derive(Component)]
-pub struct GameStateComponent {}
+pub struct GameStateComponent {
+    game_state: GameState,
+}
--- crates/demon-bluff-logic-engine/src/engine/debugger/mod.rs
+++ crates/demon-bluff-logic-engine/src/engine/debugger/mod.rs
@@ -5,11 +5,22 @@ mod hypothesis_node;
 mod node;
 mod node_type;
 
+use debug_break::DebugBreak;
+use force_graph::ForceGraph;
+
 pub use self::node::Node;
+
+use super::FitnessAndAction;
 
 pub struct Debugger {
     root_index: usize,
     nodes: Vec<Node>,
+    graph: ForceGraph<usize, Option<FitnessAndAction>>,
+    last_debug_break: Option<usize>,
 }
 
-impl Debugger {}
+impl Debugger {
+    fn handle_break(debug_break: DebugBreak) {
+        todo!()
+    }
+}
