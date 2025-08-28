93397d64b5c3d2dda9d6653f73a37937723bd8aa
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-27 21:57:33 -0400

    Remove duplicate code

--- crates/demon-bluff-logic-engine/src/engine/hypothesis/evaluator.rs
+++ crates/demon-bluff-logic-engine/src/engine/hypothesis/evaluator.rs
@@ -16,8 +16,6 @@ pub trait HypothesisEvaluator<'a, TLog, THypothesis, TDesire, FDebugBreak>:
 	HypothesisFunctions
 {
 	fn sub_evaluate(&mut self, hypothesis_reference: &HypothesisReference) -> HypothesisResult;
-	fn set_desire(&mut self, desire_reference: &DesireProducerReference, desired: bool);
-	fn desire_result(&self, desire_reference: &DesireConsumerReference) -> HypothesisResult;
 }
 
 impl<'a, TLog, FDebugBreak> HypothesisEvaluator<'a, TLog, HypothesisType, DesireType, FDebugBreak>
@@ -105,47 +103,5 @@ where
 		);
 
 		last_evaluate
-	}
-
-	fn set_desire(&mut self, desire_reference: &DesireProducerReference, desired: bool) {
-		let mut borrow = self.desire_data.borrow_mut();
-		let data = &mut borrow[desire_reference.index()];
-
-		let current_reference = self.current_reference();
-		data.pending.remove(current_reference);
-
-		if desired {
-			data.desired.insert(current_reference.clone());
-			data.undesired.remove(current_reference);
-		} else {
-			data.undesired.insert(current_reference.clone());
-			data.desired.remove(current_reference);
-		}
-
-		info!(logger: self.log, "{} Set {}: {}. Now {}", self.depth(), desire_reference, desired, data);
-	}
-
-	fn desire_result(&self, desire_reference: &DesireConsumerReference) -> HypothesisResult {
-		let definition = &self.desire_definitions[desire_reference.index()];
-		let borrow = self.desire_data.borrow();
-
-		let data = &borrow[desire_reference.index()];
-
-		info!(logger: self.log, "{} Read desire {} {} - {}", self.depth(), desire_reference, definition.desire(), data);
-		let total = data.total();
-		let fitness = FitnessAndAction::new(
-			if data.desired.is_empty() {
-				0.0 // stop divide by 0
-			} else {
-				(data.desired.len() as f64) / (total as f64)
-			},
-			None,
-		);
-		if data.pending.is_empty() {
-			HypothesisResult::Conclusive(fitness)
-		} else {
-			info!(logger: self.log, "{} Remaining Pending: {}", self.depth(), data.pending.iter().map(|producer_hypothesis_reference| format!("{producer_hypothesis_reference}")).collect::<Vec<String>>().join(", "));
-			HypothesisResult::Pending(fitness)
-		}
 	}
 }
