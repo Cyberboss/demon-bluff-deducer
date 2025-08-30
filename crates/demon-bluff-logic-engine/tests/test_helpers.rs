71d93485fe247bd2cbf2a2c5cd5ad762f0745a27
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-30 01:08:24 -0400

    Failing test for simple reveal curiosity and some cleanups and helpers

--- crates/demon-bluff-logic-engine/tests/test_helpers.rs
+++ crates/demon-bluff-logic-engine/tests/test_helpers.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, path::PathBuf};
+use std::{
+	fs::{self, File},
+	path::PathBuf,
+};
 
 use demon_bluff_gameplay_engine::game_state::GameState;
 use demon_bluff_logic_engine::{PlayerAction, predict};
@@ -11,15 +14,15 @@ pub fn test_game_state(state_name: &str, expected_outcome: PlayerAction) {
 	path.set_extension("json");
 
 	let file_reader = File::open(path.as_path())
-		.expect(format!("Unable to open game state file: {}", path.display()).as_str());
+		.unwrap_or_else(|_| panic!("Unable to open game state file: {}", path.display()));
 	let game_state: GameState = serde_json::from_reader(file_reader)
-		.expect(format!("Could not deserialize game state file: {}", path.display()).as_str());
+		.unwrap_or_else(|_| panic!("Could not deserialize game state file: {}", path.display()));
 
 	colog::init();
 
 	let log = log::logger();
 	let action = predict(&log, &game_state)
-		.unwrap_or_else(|err| panic!("Prediction error occured: {}", err));
+		.unwrap_or_else(|err| panic!("Prediction error occured: {err}"));
 
 	assert_eq!(
 		expected_outcome,
@@ -29,4 +32,14 @@ pub fn test_game_state(state_name: &str, expected_outcome: PlayerAction) {
 			.expect("There should have been at least one action!")
 	);
 	assert_eq!(1, action.len());
+}
+
+pub fn generate_state_file(state_name: &str, game_state: &GameState) {
+	let mut path = PathBuf::new();
+	path.push("tests");
+	path.push("game_states");
+	path.push(state_name);
+	path.set_extension("json");
+	fs::write(path, serde_json::to_string_pretty(&game_state).unwrap())
+		.expect("Writing out the state file failed!");
 }
