879f7276a2c49c06940211970c20b129242704f1
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-31 10:26:49 -0400

    Relative path

--- crates/demon-bluff-deducer/src/plugins/evaluator/components/game_state.rs
+++ crates/demon-bluff-deducer/src/plugins/evaluator/components/game_state.rs
@@ -10,7 +10,10 @@ pub struct GameStateComponent {
 
 impl GameStateComponent {
 	pub fn new() -> Self {
-		let file_reader = File::open("S:\\workspace\\demon-bluff-bot\\crates\\demon-bluff-logic-engine\\tests\\game_states\\gemcrafter_1_says_5_good.json").unwrap();
+		let file_reader = File::open(
+			"crates\\demon-bluff-logic-engine\\tests\\game_states\\gemcrafter_1_says_5_good.json",
+		)
+		.expect("This test json should be present in the workspace. Is your working directory the workspace root?");
 		let game_state: GameState = serde_json::from_reader(file_reader).unwrap();
 		Self { game_state }
 	}
