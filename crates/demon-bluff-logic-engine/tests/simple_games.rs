80bb8230bf6564666c9db46e599a9ecdb39fae41
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-09-03 00:24:46 -0400

    Next challenge: Learn to use an ability

--- crates/demon-bluff-logic-engine/tests/simple_games.rs
+++ crates/demon-bluff-logic-engine/tests/simple_games.rs
@@ -1,7 +1,7 @@
 use demon_bluff_gameplay_engine::{
 	Expression,
 	game_state::{Action, DrawStats, KillAttempt, KillData, KillResult, RevealResult, new_game},
-	testimony::{ConfessorClaim, Direction, Testimony},
+	testimony::{ConfessorClaim, Direction, RoleClaim, Testimony},
 	villager::{GoodVillager, Minion, VillagerArchetype, VillagerIndex, VillagerInstance},
 };
 use test_helpers::run_game;
@@ -83,5 +83,90 @@ fn simple_game_4() {
 			)),
 		],
 		None,
+	);
+}
+
+#[test]
+fn simple_game_5() {
+	let game_state = new_game(
+		vec![
+			VillagerArchetype::GoodVillager(GoodVillager::Lover),
+			VillagerArchetype::GoodVillager(GoodVillager::Gemcrafter),
+			VillagerArchetype::GoodVillager(GoodVillager::Confessor),
+			VillagerArchetype::GoodVillager(GoodVillager::Hunter),
+			VillagerArchetype::GoodVillager(GoodVillager::Medium),
+			VillagerArchetype::GoodVillager(GoodVillager::Judge),
+			VillagerArchetype::Minion(Minion::Minion),
+		],
+		DrawStats::new(5, 0, 1, 0),
+		1,
+		false,
+	);
+
+	run_game(
+		&game_state,
+		vec![
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(0),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Hunter),
+					Some(Testimony::hunter(
+						&VillagerIndex(0),
+						2,
+						game_state.total_villagers(),
+					)),
+				)),
+			)),
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(1),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Confessor),
+					Some(Expression::Leaf(Testimony::Confess(ConfessorClaim::Good))),
+				)),
+			)),
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(2),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Medium),
+					Some(Expression::Leaf(Testimony::Role(RoleClaim::new(
+						VillagerIndex(5),
+						VillagerArchetype::GoodVillager(GoodVillager::Lover),
+					)))),
+				)),
+			)),
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(3),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Judge),
+					None,
+				)),
+			)),
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(4),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Gemcrafter),
+					Some(Expression::Leaf(Testimony::Good(VillagerIndex(4)))),
+				)),
+			)),
+			Action::TryReveal(RevealResult::new(
+				VillagerIndex(5),
+				Some(VillagerInstance::new(
+					VillagerArchetype::GoodVillager(GoodVillager::Lover),
+					Some(Testimony::lover(
+						&VillagerIndex(5),
+						1,
+						game_state.total_villagers(),
+					)),
+				)),
+			)),
+			Action::TryExecute(KillAttempt::new(
+				VillagerIndex(4),
+				Some(KillResult::Revealed(
+					KillData::new(Some(VillagerArchetype::Minion(Minion::Minion)), false)
+						.expect("Bad kill data"),
+				)),
+			)),
+		],
+		Some(6),
 	);
 }
